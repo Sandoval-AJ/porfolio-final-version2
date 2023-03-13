d2d6717cbf1d4c3aa54198899278fc00237a46f9
Sandoval Agustin <sando94_27@hotmail.com>
2023-03-13 07:02:19 -0300

    agregue un reloj y una galeria de proyectos

--- javascript.js
+++ javascript.js
@@ -8,3 +8,21 @@ open.addEventListener("click", function(){
 close.addEventListener("click", function(){
     modal.style.display = "none";
 })
+//reloj o intento de reloj
+const currentTime = () => {
+    const el = document.getElementById("time");
+
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    
+    hh = hh < 10 ? `0${hh}` : hh;
+    mm = mm < 10 ? `0${mm}` : mm;
+    ss = ss < 10 ? `0${ss}` : ss;
+
+    let time = `${hh}:${mm}:${ss}`;
+    el.innerText = time;
+};
+currentTime();
+setInterval(currentTime, 1000);
